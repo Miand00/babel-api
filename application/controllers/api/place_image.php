75ee5ecb7805ae982b8c29cb0ab5ab4f6631d8e9
Miand00 <mikhael.andrikho@phincon.com>
2020-11-30 16:46:23 +0700

    API Place Image

---
+++ application/controllers/api/place_image.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class place_image extends REST_Controller{
+  function __construct($config = 'rest') {
+    parent::__construct($config);
+    $this->load->database();
+    $this->load->model(array("api/place_image_model"));
+    $this->load->library(array("form_validation"));
+    $this->load->helper("security");
+  }
+
+  function index_get(){
+    $id = $this->get('id');
+    $places_image = $this->place_image_model->get_place_images($id);
+
+    if(count($places_image)>0){
+      $this->response(array(
+        "status" => 1,
+        "message" => "Image found",
+        "data" => $places_image
+      ), REST_Controller::HTTP_OK);
+    }else{
+      $this->response(array(
+        "status" => 0,
+        "message" => "No image found",
+        "data" => $places_image
+      ), REST_Controller::HTTP_NOT_FOUND);
+    }
+  }
+}
+?>
---
+++ application/models/api/place_image_model.php
@@ -0,0 +1,16 @@
+<?php
+
+class place_image_model extends CI_Model{
+  public function __construct(){
+    parent::__construct();
+    $this->load->database();
+  }
+
+  public function get_place_images($id){
+    $this->db->where('places_id', $id);
+    $query = $this->db->get('places_images')->result();
+    return $query;
+  }
+}
+
+?>
