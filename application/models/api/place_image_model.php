48c1ef4f2d70ebf9f53d98a39347d8ae01736d06
Miand00 <mikhael.andrikho@phincon.com>
2020-12-08 20:36:39 +0700

    Conflict fix 1

--- application/models/api/place_image_model.php
+++ application/models/api/place_image_model.php
@@ -7,7 +7,7 @@ class place_image_model extends CI_Model{
   }
 
   public function get_place_images($id){
-    $this->db->where('places_id', $id);
+    $this->db->where('place_id', $id);
     $query = $this->db->get('places_images')->result();
     return $query;
   }
